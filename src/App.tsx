58f1a2bd2210c04ee16e58190d5ea44277355091
tornike jandieri <tornikejandieri7@gmail.com>
2022-10-26 20:50:24 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FormEvent, useState } from 'react'
+import { AccountForm } from './AccountForm'
+import { AddressForm } from './AddressForm'
+import { useCustomHook } from './useCustumHook'
+import { UserForm } from './UserForm'
+
+type FormData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
+
+const initialData: FormData = {
+  firstName: '',
+  lastName: '',
+  age: '',
+  street: '',
+  city: '',
+  state: '',
+  zip: '',
+  email: '',
+  password: ''
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(initialData)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  function updateFields(fields: Partial<FormData>){
+    setData(prev => {
+      return {...prev, ...fields}
+    })
+  }
+  const { steps, currentStepIndex, step, isFirstStep, back, next, isLastStep } = useCustomHook([ <UserForm {...data} updateFields={updateFields} />, <AddressForm {...data} updateFields={updateFields} />, <AccountForm {...data} updateFields={updateFields} /> ])
+
+  function onSubmit(e: FormEvent){
+    e.preventDefault()
+    if(!isLastStep) return next()
+    
+  }
+
+  return <div style={{
+    position: 'relative',
+    background: 'white',
+    border: '1px solid black',
+    padding: '2rem',
+    margin: '1rem',
+    borderRadius: '.5rem',
+    fontFamily: 'Arial',
+    maxWidth: 'max-content'
+  }}>
+    <form onSubmit={onSubmit}>
+      <div style={{
+      position: 'absolute',
+      top: '.5rem',
+      right: '.5rem'
+       }}>
+        {currentStepIndex + 1} / {steps.length}
+       </div>
+       {step}
+       <div style={{
+        marginTop: '1rem',
+        display: 'flex',
+        gap: '5rem',
+        justifyContent: 'flex-end'
+       }}>
+        {isFirstStep && <button type='button' onClick={back}>Back</button>}
+        <button type='submit'>
+          {isLastStep ? 'Finish' : 'Next'}
+          </button>
+       </div>
+    </form>
+  </div>
 }
 
 export default App
