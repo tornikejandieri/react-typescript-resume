fb0cee46492d651e5a9649d840bad4fd02941032
tornike jandieri <tornikejandieri7@gmail.com>
2022-10-26 22:06:50 +0400

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { AccountForm } from './AccountForm'
 import { AddressForm } from './AddressForm'
 import { useCustomHook } from './useCustumHook'
 import { UserForm } from './UserForm'
+import { WelcomeScreen } from './WelcomeScreen'
+
+
 
 type FormData = {
   firstName: string
@@ -37,42 +40,45 @@ function App() {
       return {...prev, ...fields}
     })
   }
-  const { steps, currentStepIndex, step, isFirstStep, back, next, isLastStep } = useCustomHook([ <UserForm {...data} updateFields={updateFields} />, <AddressForm {...data} updateFields={updateFields} />, <AccountForm {...data} updateFields={updateFields} /> ])
+  const { steps, currentStepIndex, step, isFirstStep, back, next, isLastStep } = useCustomHook([ <WelcomeScreen />, <UserForm {...data} updateFields={updateFields} />, <AddressForm {...data} updateFields={updateFields} />, <AccountForm {...data} updateFields={updateFields} /> ])
 
   function onSubmit(e: FormEvent){
     e.preventDefault()
     if(!isLastStep) return next()
-    
+    //api request here
   }
 
   return <div style={{
     position: 'relative',
-    background: 'white',
-    border: '1px solid black',
+    background: '#A18CD1',
+    border: 'none',
     padding: '2rem',
     margin: '1rem',
     borderRadius: '.5rem',
-    fontFamily: 'Arial',
+    fontFamily: 'Trebuchet MS',
     maxWidth: 'max-content'
   }}>
     <form onSubmit={onSubmit}>
       <div style={{
-      position: 'absolute',
-      top: '.5rem',
-      right: '.5rem'
+          position: 'absolute',
+          top: '.5rem',
+          right: '.5rem',
+          
        }}>
         {currentStepIndex + 1} / {steps.length}
-       </div>
+      </div>
+
        {step}
+
        <div style={{
-        marginTop: '1rem',
-        display: 'flex',
-        gap: '5rem',
-        justifyContent: 'flex-end'
+            marginTop: '1rem',
+            display: 'flex',
+            gap: '5rem',
+            justifyContent: 'flex-end'
        }}>
-        {isFirstStep && <button type='button' onClick={back}>Back</button>}
-        <button type='submit'>
-          {isLastStep ? 'Finish' : 'Next'}
+          {isFirstStep && <button type='button' onClick={back}>Back</button>}
+          <button type='submit'>
+            {isLastStep ? 'Finish' : 'Next'}
           </button>
        </div>
     </form>
---
+++ src/WelcomeScreen.tsx
@@ -0,0 +1,9 @@
+
+export function WelcomeScreen(){
+  return (
+    <>
+      <h2>Welcome To The Online CV Maker</h2>
+      
+    </>
+  )
+}
