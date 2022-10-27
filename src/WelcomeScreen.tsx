58af83f0c16c17ea670673ad272bbdbe19fb9068
tornike jandieri <tornikejandieri7@gmail.com>
2022-10-27 13:42:07 +0400

    logos

--- src/WelcomeScreen.tsx
+++ src/WelcomeScreen.tsx
@@ -1,8 +1,27 @@
+import Logo from './images/react-logo.png'
+import LogoTwo from './images/vite-logo.png'
 
 export function WelcomeScreen(){
   return (
     <>
-      <h2>Welcome To The Online CV Maker</h2>
+      <h2 style={{marginBottom: '40px'}}>Welcome To The Online CV Maker</h2>
+     <span>Powered By: </span>
+     <div className='images'>
+     <img src={Logo} className='react-logo' style={{width: '150px',
+      display: 'flex',
+      justifyContent: 'center',
+       alignItems: 'center',
+       alignSelf: 'center',
+      maxWidth: 'max-content',
+        }} />
+         <img src={LogoTwo} style={{width: '150px',
+      display: 'flex',
+      justifyContent: 'center',
+       alignItems: 'center',
+       alignSelf: 'center',
+      maxWidth: 'max-content',
+        }} />
+     </div>
       
     </>
   )
